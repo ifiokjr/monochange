d02a2f3b3a788b45fb3faeea33e282e8d95e67f1
Ifiok Jr <ifiokotung@gmail.com>
2023-04-12 18:00:39 +0100

    refactor(mdt): move mdt to new repository
    https://github.com/ifiokjr/mdt

--- crates/mdt_lsp/src/error.rs
+++
@@ -1,9 +0,0 @@
-use thiserror::Error as ThisError;
-
-#[derive(Debug, ThisError, Clone)]
-pub enum Error {
-  #[error("failure to load markdown: {0}")]
-  MarkdownError(String),
-}
-
-pub type Result<T> = std::result::Result<T, Error>;
