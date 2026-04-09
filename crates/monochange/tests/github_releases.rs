49e33e7552b31f21fbfced3ee533e73c071e1a8a
Ifiok Jr. <ifiokotung@gmail.com>
2026-04-09 09:09:26 +0100

    refactor: use insta snapshots for scenario outputs

--- crates/monochange/tests/github_releases.rs
+++ crates/monochange/tests/github_releases.rs
@@ -1,8 +1,9 @@
+use insta::assert_json_snapshot;
 use rstest::rstest;
 
 mod test_support;
 use test_support::{
-	assert_json_fixture, expected_fixture_path, run_json_command, setup_scenario_workspace,
+	current_test_name, run_json_command, setup_scenario_workspace, snapshot_settings,
 };
 
 #[rstest]
@@ -10,12 +11,13 @@ use test_support::{
 #[case::ungrouped("ungrouped")]
 #[case::custom_sections("custom-sections")]
 #[case::default_custom_sections("default-custom-sections")]
-fn publish_github_release_dry_run_matches_expected_fixture(#[case] scenario: &str) {
+fn publish_github_release_dry_run_matches_snapshot(#[case] scenario: &str) {
+	let mut settings = snapshot_settings();
+	settings.set_snapshot_suffix(current_test_name());
+	let _guard = settings.bind_to_scope();
+
 	let scenario_relative = format!("github-releases/{scenario}");
 	let tempdir = setup_scenario_workspace(&scenario_relative);
 	let json = run_json_command(tempdir.path(), "publish-release", Some("2026-04-06"));
-	assert_json_fixture(
-		&json,
-		&expected_fixture_path(&scenario_relative, "publish-release.json"),
-	);
+	assert_json_snapshot!(json);
 }
--- crates/monochange/tests/source_providers.rs
+++ crates/monochange/tests/source_providers.rs
@@ -1,25 +1,26 @@
+use insta::assert_json_snapshot;
 use rstest::rstest;
 
 mod test_support;
 use test_support::{
-	assert_json_fixture, expected_fixture_path, run_json_command, setup_scenario_workspace,
+	current_test_name, run_json_command, setup_scenario_workspace, snapshot_settings,
 };
 
 #[rstest]
-#[case::gitlab_publish_release("source/gitlab", "publish-release", "publish-release.json")]
-#[case::gitea_release_pr("source/gitea", "release-pr", "release-pr.json")]
-#[case::github_publish_release("source/github", "publish-release", "publish-release.json")]
-#[case::github_release_pr("source/github", "release-pr", "release-pr.json")]
-#[case::github_release_comments("source/github", "release-comments", "release-comments.json")]
-fn source_provider_scenarios_match_expected_fixture(
+#[case::gitlab_publish_release("source/gitlab", "publish-release")]
+#[case::gitea_release_pr("source/gitea", "release-pr")]
+#[case::github_publish_release("source/github", "publish-release")]
+#[case::github_release_pr("source/github", "release-pr")]
+#[case::github_release_comments("source/github", "release-comments")]
+fn source_provider_scenarios_match_snapshot(
 	#[case] scenario_relative: &str,
 	#[case] command: &str,
-	#[case] expected_name: &str,
 ) {
+	let mut settings = snapshot_settings();
+	settings.set_snapshot_suffix(current_test_name());
+	let _guard = settings.bind_to_scope();
+
 	let tempdir = setup_scenario_workspace(scenario_relative);
 	let json = run_json_command(tempdir.path(), command, Some("2026-04-06"));
-	assert_json_fixture(
-		&json,
-		&expected_fixture_path(scenario_relative, expected_name),
-	);
+	assert_json_snapshot!(json);
 }
